1b507a97800272d69703eec77f3aa5039e08d5f1
Dakota Lillie <dakota.lillie@icloud.com>
2021-05-02 10:09:48 -0700

    refactor: rename to DocumentWithStyles
    Signed-off-by: Dakota Lillie <dakota.lillie@icloud.com>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ import { ServerStyleSheet } from 'styled-components'
  * Bootstrap styled-component as in this example:
  * https://github.com/vercel/next.js/blob/86160a5190c50ea315c7ba91d77dfb51c42bc65f/examples/with-styled-components/pages/_document.js
  */
-export default class MyDocument extends Document {
+export default class DocumentWithStyles extends Document {
   static async getInitialProps(
     ctx: DocumentContext
   ): Promise<DocumentInitialProps> {
