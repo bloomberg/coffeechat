50aad1ffa11abef30541e2082bbe376196e7925d
vgarciavalen <info@vgarcia.dev>
2021-05-02 05:36:36 -0400

    chore(nextjs): add custom doc comment
    Signed-off-by: vgarciavalen <info@vgarcia.dev>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
+/**
+ * Bootstrap styled-component as in this example:
+ * https://github.com/vercel/next.js/blob/86160a5190c50ea315c7ba91d77dfb51c42bc65f/examples/with-styled-components/pages/_document.js
+ */
 export default class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
