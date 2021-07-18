bb5f8691bbae1c41a8f06f744298098fdba3b5bc
vgarciavalen <info@vgarcia.dev>
2021-07-18 05:45:22 -0400

    chore(npm): update dev packages
    Signed-off-by: vgarciavalen <info@vgarcia.dev>

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.spec.[jt]s?(x)'],
   setupFiles: ['<rootDir>/.jest/storyBookTesting.ts'],
   setupFilesAfterEnv: ['<rootDir>/.jest/jestDomSetup.ts'],
+  testEnvironment: 'jsdom',
 }
