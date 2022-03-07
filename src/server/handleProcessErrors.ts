83342ea253d07874479b7b28527a62df8b3d2144
vgarciavalen <info@vgarcia.dev>
2022-03-07 05:49:24 -0500

    chore(npm): update dependencies
    - chore(npm): updated prisma to latest
    - chore(npm): update pino
    - fix(pino): remove unnecessary code
    - chore(npm): update nextjs
    - chore(npm): update eslint
    - chore(npm): update typescript and eslint
    - chore(npm): update types
    
    Signed-off-by: vgarciavalen <info@vgarcia.dev>

--- src/server/handleProcessErrors.ts
+++ src/server/handleProcessErrors.ts
@@ -1,26 +1,23 @@
-import log, { final } from './log'
+import log from './log'
 
-const signalHandler = final((_, finalLogger, event_) => {
-  finalLogger.info({ evt: event_ }, 'caught')
+const signalHandler = (event: string): void => {
+  log.info({ evt: event }, 'caught')
   // This will provide normal termination
   // based on pino docs: https://github.com/pinojs/pino/blob/HEAD/docs/asynchronous.md
   // eslint-disable-next-line unicorn/no-process-exit
   process.exit(0)
+}
+
+process.on('SIGQUIT', () => signalHandler('SIGQUIT'))
+process.on('SIGTERM', () => signalHandler('SIGTERM'))
+process.on('SIGINT', () => signalHandler('SIGINT'))
+process.on('beforeExit', () => signalHandler('beforeExit'))
+process.on('exit', () => signalHandler('exit'))
+
+process.on('uncaughtException', (error) => {
+  log.fatal({ err: error }, 'unhandled')
+  process.exit(1)
 })
-
-process.on('SIGQUIT', () => signalHandler(null, 'SIGQUIT'))
-process.on('SIGTERM', () => signalHandler(null, 'SIGTERM'))
-process.on('SIGINT', () => signalHandler(null, 'SIGINT'))
-process.on('beforeExit', () => signalHandler(null, 'beforeExit'))
-process.on('exit', () => signalHandler(null, 'exit'))
-
-process.on(
-  'uncaughtException',
-  final((error, finalLogger) => {
-    finalLogger.fatal({ err: error }, 'unhandled')
-    process.exit(1)
-  })
-)
 
 // the node api is Error | any
 // per the docs https://nodejs.org/api/process.html#process_event_unhandledrejection
